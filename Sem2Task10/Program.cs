bbda1bac7eb2f267037637d78400ceac36be7c4a
Gofra Maslow <goframaslow@gmail.com>
2023-01-19 20:52:17 +0300

    Sem2

---
+++ Sem2Task10/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите число: ");
+//парсим
+int num = int.Parse(Console.ReadLine() ?? "0");
+
+int length = num.ToString().Length;
+//проверка
+if (length == 3)
+{
+    //последнюю убираем 
+    int res = num / 10;
+
+    res = res % 10;
+   
+    Console.WriteLine("Вторая цифра : " + res);
+}
+else
+{
+    Console.WriteLine("Вы не ввели Трехзначное число!");
+}
---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите  трехзначное число: ");
+
+char[] digit = Console.ReadLine().ToCharArray();
+
+if (digit.Length > 2)
+{
+    Console.WriteLine("Третья цифра : " + digit[2]);
+}
+else
+{
+    Console.WriteLine("Третьей нет!");
+}

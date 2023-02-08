c62a5dc2e857be84c7fec8459474f2d6a38ea743
Gofra Maslow <goframaslow@gmail.com>
2023-02-08 14:07:08 +0300

    dz_54_56_58

---
+++ Sem8Task58/Program.cs
@@ -0,0 +1,53 @@
+﻿void Fill2DArray(int[,] matr, int min, int max)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+}
+
+
+int[,] MultMatrx(int[,] arr1, int[,] arr2)
+{
+    int[,] arr = new int[arr1.GetLength(0),arr2.GetLength(1)];
+
+    for (int i = 0; i < arr1.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr2.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr2.GetLength(0); k++)
+            {
+                arr[i, j] += arr1[i, k] * arr2[k, j] ;
+            }
+        }
+
+    }
+    return arr;
+}
+
+// Печать двумерного массива
+void Print2DArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    
+}
+
+int[,] arrayFirst = new int[2, 2];
+Fill2DArray(arrayFirst, 0, 15);
+int[,] arraySecond = new int[2, 2];
+Fill2DArray(arraySecond, 0, 15);
+int[,] matrixMult=MultMatrx(arrayFirst, arraySecond);
+Print2DArray(arrayFirst);
+Print2DArray(arraySecond);
+Print2DArray(matrixMult);

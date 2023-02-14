48271f080928101cfecb46f830e39ce61e340eef
Gofra Maslow <goframaslow@gmail.com>
2023-02-14 09:51:41 +0300

    dz_64_66_68

---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,31 @@
+﻿int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Метод, принимает строку, выводит в консоль
+void PrintResult(int numberM,int numberN )
+{
+    if  (numberM< numberN) 
+    {
+       Console.WriteLine($"Сумма в промежутке M...N = {SumRecMN(numberM,numberN)}");
+    }
+    if (numberM> numberN) 
+    {
+        Console.WriteLine ( $"Сумма в промежутке M...N = {SumRecMN(numberN, numberM)}");
+    }
+}
+
+int SumRecMN(int numberM, int numberN)
+{
+    if (numberM >= numberN) return numberN;
+   return  numberM + SumRecMN(numberM + 1, numberN);
+}
+
+
+int numberM = ReadData("Введите числа M: ");
+int numberN = ReadData("Введите числа N: ");
+
+PrintResult(numberM, numberN);
---
+++ Sem9Task68/Program.cs
@@ -0,0 +1,41 @@
+﻿int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// печать результата
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+// сумма чисел от m до n
+int AckermannFunc(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (m > 0 && n == 0)
+    {
+        return AckermannFunc(m - 1, 1);
+    }
+    else
+    {
+        return AckermannFunc(m - 1, AckermannFunc(m, n - 1));
+    }
+}
+
+
+int numM = ReadData("Введите неотрицательное число m: ");
+int numN = ReadData("Введите неотрицательное число n: ");
+if (numM >= 0 && numN >= 0)
+{
+    PrintResult($"Функция Аккермана A({numM},{numN}) = {AckermannFunc(numM, numN)}");
+}
+else 
+{
+    PrintResult("Необходимо использовать неотрицательные числа.");
+}
